0928e06bf53b728c7e2dc4ed7bc0e62334a0d693
diego-hernandez-jimenez <diego.her.jimenez@gmail.com>
2026-08-26 19:02:06 +0200

    feat: flatten repo structure, remove src/

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 """A Google Cloud + Neon Python Pulumi plugin"""
 
 from components.gcp_mlflow import GcpMlflow  # noqa: F401
-from components.src.artifact_store import ArtifactStore  # noqa: F401
-from components.src.backend_store import BackendStore  # noqa: F401
-from components.src.iam import MlflowIam  # noqa: F401
-from components.src.mlflow_service.tracking_server import MlflowServer  # noqa: F401
+from components.artifact_store import ArtifactStore  # noqa: F401
+from components.backend_store import BackendStore  # noqa: F401
+from components.iam import MlflowIam  # noqa: F401
+from components.mlflow_service.tracking_server import MlflowServer  # noqa: F401
